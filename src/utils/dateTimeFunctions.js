ae971e1c88b33473baad7e8a9c37dc3ddc06a426
illya bondar <1816579@gmail.com>
2022-09-28 22:23:11 +0300

    new date and time convertion functions

--- src/utils/dateTimeFunctions.js
+++ src/utils/dateTimeFunctions.js
@@ -2,3 +2,21 @@ export const showTime = UTCString => {
   const date = new Date(UTCString);
   return date.toLocaleTimeString([], { hour12: false });
 };
+
+export const showDate = UTCString => {
+  const date = new Date(UTCString);
+  const stringifiedDate = `${date.getDate().toString().padStart(2, '0')}.${(
+    date.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}.${date.getFullYear()}`;
+  return stringifiedDate;
+};
+
+export const showUnderlinedDate = UTCString => {
+  const date = new Date(UTCString);
+  const stringifiedDate = `${date.getFullYear()}_${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, '0')}_${date.getDate().toString().padStart(2, '0')}`;
+  return stringifiedDate;
+};
