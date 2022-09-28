008483120cb1a9a29f91d91c04fbf5f705e020b7
illya bondar <1816579@gmail.com>
2022-09-28 22:22:30 +0300

    fix: markup height

--- src/components/UtilsMarkup/UtilsMarkup.styled.js
+++ src/components/UtilsMarkup/UtilsMarkup.styled.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   margin: 0;
   padding: 0;
+  height: 100vh;
   max-height: 100vh;
   width: 100%;
 `;
